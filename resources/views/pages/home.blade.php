4ec00b155dd1c2273d5b4b48056502efdbfd7cba
Dev <dev@Nehmes-iMac.local>
2016-10-11 18:00:02 +0400

    Working on the homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.home')
 
 @section('content')
+	<div class="Slideshow">
+		<div class="Slides">
+			<div class="Slide centered-xy">
+				<div class="Slide__content">
+					<h2 class="Slide__title">Launching soon</h2>
+					<p class="Slide__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed</p>
+				</div>
+			</div>
+		</div>
 
+
+	</div>
 @endsection
