6f944d5b25c71de641add3a2e866a7d8b7706ebe
Dev <dev@Nehmes-iMac.local>
2016-10-11 17:43:20 +0400

    Initial commit

---
+++ tests/PagesTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PagesTest extends TestCase
+{
+	use DatabaseMigrations;
+
+    public function test_it_displays_the_homepage()
+    {
+    	$this->visit('/')
+    		->see('Villa Nova Global');
+    }
+}
