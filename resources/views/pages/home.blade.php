255804b78f75e5e996e79f74ac3bcd780dc8863e
Mark <mark@Marks-MacBook-Pro.local>
2016-10-11 22:27:40 +0400

    Working on the homomepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-<div class="homepage-hero-module">
+{{-- <div class="homepage-hero-module">
     <div class="video-container">
         <div class="filter"></div>
-        <video autoplay loop class="fillWidth">
+        <video autoplay loop class="fillWidth" muted>
             <source src="/videos/villanova.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
         </video>
         <div class="poster hidden">
             <img src="/images/slides/slide1.jpg" alt="Slide 1" title="Slide 1" />
         </div>
     </div>
-</div>
-{{-- 	<div class="Slideshow">
+</div> --}}
+	<div class="Slideshow">
 		<div class="Slides">
 			<div class="Slide centered-xy">
 				<div class="Slide__content">
@@ -21,8 +21,6 @@
 					<p class="Slide__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed</p>
 				</div>
 			</div>
-		</div> --}}
-
-
+		</div>
 	</div>
 @endsection
