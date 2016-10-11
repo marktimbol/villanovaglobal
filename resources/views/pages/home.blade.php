9c0fb5ef960aa3d6f208f5c02d3b9805d677c593
Mark <mark@Marks-MacBook-Pro.local>
2016-10-11 22:02:05 +0400

    Homepage video

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.home')
 
 @section('content')
-	<div class="Slideshow">
+
+<div class="homepage-hero-module">
+    <div class="video-container">
+        <div class="filter"></div>
+        <video autoplay loop class="fillWidth">
+            <source src="/videos/villanova.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+        </video>
+        <div class="poster hidden">
+            <img src="/images/slides/slide1.jpg" alt="Slide 1" title="Slide 1" />
+        </div>
+    </div>
+</div>
+{{-- 	<div class="Slideshow">
 		<div class="Slides">
 			<div class="Slide centered-xy">
 				<div class="Slide__content">
@@ -9,7 +21,7 @@
 					<p class="Slide__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed</p>
 				</div>
 			</div>
-		</div>
+		</div> --}}
 
 
 	</div>
